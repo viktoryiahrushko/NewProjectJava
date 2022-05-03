9c455e9854755d8b233c30c3313980e901384191
Victoria Vysotsky <viktoryiahrushko@gmail.com>
2022-05-02 22:49:28 -0700

    1 commit

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -4,7 +4,7 @@ public class AscendingSequence {
 
     public int[] ascendingSequenceAlgorithm(int start, int end, int step) {
         if (step > 0 && start < end) {
-            int[] result = new int[(end - start + 1) / step];
+            int[] result = new int[((end - start) / step) + 1];
 
 
             for (int i = 0; i < result.length; i ++) {
